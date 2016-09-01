9767908565bdd2f63a7ac43b34206a6e7ae0a010
Tino <lubeskit@gmai.com>
2016-09-01 18:57:44 -0500

    system script updated

--- system.sh
+++ system.sh
@@ -11,7 +11,9 @@ echo y | pacman -S xfce4
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 echo "Done. Don't forget to edit .xinitrc and /etc/slim.conf"
-
+echo ".xinitrc -> Comment from twm to xterm -name login"
+echo "... and add: exec xfce4-session"
+echo "Now at /etc/slim.conf change current_theme to something better ffs."
 
 
 
